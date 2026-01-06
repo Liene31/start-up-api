d6567154d7ebc42fc215d70d9ed1376745604d46
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-01-06 21:51:11 +0100

    refactor code

---
+++ controllers/getAllData.js
@@ -0,0 +1,42 @@
+import { startups } from "../data/data.js";
+
+export const getAllData = (req, res) => {
+  let filteredData = startups;
+
+  const { industry, country, continent, is_seeking_funding, has_mvp } =
+    req.query;
+
+  if (industry) {
+    filteredData = filteredData.filter((data) => {
+      return data.industry.toLowerCase() === industry.toLowerCase();
+    });
+  }
+
+  if (country) {
+    filteredData = filteredData.filter((data) => {
+      return data.country.toLowerCase() === country.toLowerCase();
+    });
+  }
+
+  if (continent) {
+    filteredData = filteredData.filter((data) => {
+      return data.continent.toLowerCase() === continent.toLowerCase();
+    });
+  }
+
+  if (is_seeking_funding) {
+    filteredData = filteredData.filter((data) => {
+      return (
+        data.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+      );
+    });
+  }
+
+  if (has_mvp) {
+    filteredData = filteredData.filter((data) => {
+      return data.has_mvp === JSON.parse(has_mvp.toLowerCase());
+    });
+  }
+
+  res.json(filteredData);
+};
--- server.js
+++ server.js
@@ -1,49 +1,17 @@
 import express from "express";
-import { startups } from "./data/data.js";
+import { apiRouter } from "./routes/apiRouts.js";
+import cors from "cors";
 
 const PORT = 8000;
 
 const server = express();
 
-server.get("/api", (req, res) => {
-  let filteredData = startups;
+server.use(cors());
 
-  const { industry, country, continent, is_seeking_funding, has_mvp } =
-    req.query;
+server.use("/api", apiRouter);
 
-  if (industry) {
-    filteredData = filteredData.filter((data) => {
-      return data.industry.toLowerCase() === industry.toLowerCase();
-    });
-  }
-
-  if (country) {
-    filteredData = filteredData.filter((data) => {
-      return data.country.toLowerCase() === country.toLowerCase();
-    });
-  }
-
-  if (continent) {
-    filteredData = filteredData.filter((data) => {
-      return data.continent.toLowerCase() === continent.toLowerCase();
-    });
-  }
-
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter((data) => {
-      return (
-        data.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-      );
-    });
-  }
-
-  if (has_mvp) {
-    filteredData = filteredData.filter((data) => {
-      return data.has_mvp === JSON.parse(has_mvp.toLowerCase());
-    });
-  }
-
-  res.json(filteredData);
+server.use((req, res) => {
+  res.status(404).json({ message: "Endpoint not found" });
 });
 
 server.listen(PORT, () => {
